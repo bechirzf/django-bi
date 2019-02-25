6504d162c3410d3464efde1e8e437a167b989861
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-02-25 13:35:38 +0500

    first commit

---
+++ reporting/tests/fixtures/objects/dashboards/dummy1/dashboard.py
@@ -0,0 +1,8 @@
+from reporting.models.dashboard import BaseDashboard
+
+
+class Dashboard(BaseDashboard):
+    """
+    Пример доски для тестов и образца. Не менять!
+    """
+    title = 'Dummy board 1'

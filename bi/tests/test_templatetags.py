9413f3c4d946e0f46c582a655ce452a4f605d005
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-02-28 12:02:36 +0500

    tested report template tag

--- bi/tests/test_templatetags.py
+++ bi/tests/test_templatetags.py
@@ -1,6 +1,9 @@
+from django.http import QueryDict
 from django.test import TestCase
+from django.urls import reverse
 
 from bi.templatetags.gravatar import gravatar_url, gravatar
+from bi.tests.fixtures.objects.reports.dummy1.report import Report as DummyReport1
 
 
 class TemplateTagsTests(TestCase):
@@ -15,3 +18,7 @@ class TemplateTagsTests(TestCase):
             gravatar('zhelyabuzhsky@icloud.com', 160),
             '<img src="https://www.gravatar.com/avatar/0284c304e6e8f0de6cfa8d7bed45d3aa?s=160" height="160" width="160">'
         )
+
+    def test_report(self):
+        response = self.client.get(reverse('bi:dashboard-detail', args=('dummy1',)))
+        self.assertTemplateUsed(response, DummyReport1(QueryDict()).template)

c9b8d29f305cec4c26c403338dfe30d7a297add6
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-02-28 14:39:22 +0500

    changed fixture dashboards

--- bi/tests/fixtures/objects/dashboards/dummy1/dummy3/dashboard.py
+++ bi/tests/fixtures/objects/dashboards/dummy1/dummy3/dashboard.py
@@ -2,7 +2,4 @@ from bi.models.dashboard import BaseDashboard
 
 
 class Dashboard(BaseDashboard):
-    """
-    Пример доски для тестов и образца. Не менять!
-    """
     title = 'Dummy board 3'
--- bi/tests/test_views.py
+++ bi/tests/test_views.py
@@ -6,5 +6,5 @@ class DashboardsViewTests(TestCase):
     def test_dashboard_detail(self):
         response = self.client.get(reverse('bi:dashboard-detail', args=('dummy1',)))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Первый отчёт на дашборде")
-        self.assertContains(response, "Второй отчёт на дашборде")
+        self.assertContains(response, 'Dummy report 1')
+        self.assertContains(response, 'Dummy report 2')

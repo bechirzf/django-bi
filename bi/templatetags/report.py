c16bcfc4cc8bb0b12f6b0fdd5ad89fcc5ae8e93b
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-03-11 10:47:10 +0500

    improved report template tag

--- bi/templatetags/report.py
+++ bi/templatetags/report.py
@@ -8,7 +8,7 @@ register = Library()
 
 @register.simple_tag
 def report(report_class):
-    rprt = get_entity_by_path(report_class, 'Report', QueryDict())
+    rprt = get_entity_by_path('reports/{}'.format(report_class), 'Report', QueryDict())
     context = {'report': rprt}
     template = loader.get_template('bi/dashboard_report.html')
     return template.render(context)
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class PyTest(TestCommand):
 
 setup(
     name='django-bi',
-    version='1.0.11',
+    version='1.0.12',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     license='GPLv3 License',

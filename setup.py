1719d22d6641173941738377d98166a4e7e44434
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-04-05 15:15:04 +0500

    added favicon

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class PyTest(TestCommand):
 
 setup(
     name='django-bi',
-    version='1.0.28',
+    version='1.0.29',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     license='GPLv3 License',
